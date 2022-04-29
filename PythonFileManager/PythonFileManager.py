8c8ad1ef40d562b1ce50aea466822f3733f49ece
mconsulting <mconsulting@users.noreply.github.com>
2022-04-28 21:31:04 -0500

    First file move using python shutil.copy from wip

--- PythonFileManager/PythonFileManager.py
+++ PythonFileManager/PythonFileManager.py
@@ -1,10 +1,15 @@
 import os
 import pandas as pd
+import shutil
 
 
-source_path=os.getcwd()
-os.mkdir(source_path + "\\files")
-print(os.listdirs())
+source_path=r"C:\Users\mason\Mason Consulting\Mason Prepaid - Documents\civil rights\wip"
+target_path=os.getcwd() + "\\files"
+files_to_copy=os.listdir(source_path)
 
 
 
+for f in files_to_copy:
+    print(f)
+    new_file=shutil.copy(source_path + "\\" + f,target_path)
+    print(new_file)
